9181d2de141e06e33993135695bd76596ff95baa
Vikas <mailatvikasmodi@gmail.com>
2022-11-23 02:18:41 -0500

    order clerk add

---
+++ lib/OrderClerk/Client.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(client());
+}
+
+class client extends StatefulWidget{
+  _clientState createState() => _clientState();
+}
+
+
+
+class _clientState extends State<client> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF7AB4BD),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+        title: const Text(
+          'Order Clerk',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 22.0,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(padding: const EdgeInsets.only(top: 60.0),
+            child: Center(
+              child: Container(
+                  color: Color(0xFF4F7F84),
+                  width: double.infinity,
+                  height: 150,
+                  margin: EdgeInsets.only(bottom: 50),
+                  /*decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(50.0)),*/
+                  child: Image.asset('assets/Logo.png')
+              ),
+            ),
+            ),
+          ],
+        )
+      ),
+    );
+  }
+}
+
+class menuoption extends StatelessWidget {
+  const menuoption(
+      {required this.menutext, this.padlef = 0.0, this.autofocus = false});
+
+  final String menutext;
+  final double padlef;
+  final bool autofocus;
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(padlef, 5.0, 0.0, 5.0),
+        child: TextButton(
+          autofocus: autofocus,
+          onPressed: () {},
+          child: Text(
+            menutext,
+            style: TextStyle(fontSize: 17.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'Stock_Manager/Stock_Products.dart';
 import 'Login.dart';
+import 'OrderClerk/Client.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -15,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Login(),
+      home: client(),
     );
   }
 }
