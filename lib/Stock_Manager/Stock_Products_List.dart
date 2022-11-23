ffa2a58c4e70c2404e8e8baba8de595f6543fc7f
Hardik_5894 <Vaviya.365@gmail.com>
2022-11-23 13:43:42 -0500

    Page title is added to product list page of stock manager

--- lib/Stock_Manager/Stock_Products_List.dart
+++ lib/Stock_Manager/Stock_Products_List.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../reusable/screentitle.dart';
+
 class stockproductslist extends StatefulWidget {
   @override
   State<stockproductslist> createState() => _stockproductlistsstate();
@@ -10,8 +12,33 @@ class _stockproductlistsstate extends State<stockproductslist> {
   @override
   Widget build(BuildContext context) {
    return Scaffold(
+     backgroundColor: Color(0xFF7ab4bd),
      body: SingleChildScrollView(
-
+        child: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                const screentitle(
+                  title: 'STOCK MANAGER',
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  height: 70.0,
+                  child: SizedBox(
+                      child: TextButton(
+                        onPressed: () {},
+                        style: TextButton.styleFrom(backgroundColor: Colors.red),
+                        child: const Text(
+                          'Logout',
+                          style: TextStyle(fontSize: 17.0, color: Colors.black87),
+                        ),
+                      )),
+                ),
+              ],
+            ),
+          ],
+        ),
      ),
    );
   }
