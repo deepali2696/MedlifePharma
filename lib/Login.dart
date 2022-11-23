2b78902217229bc892511a5f0236508e42cdc3be
Vikas <mailatvikasmodi@gmail.com>
2022-11-23 01:06:58 -0500

    Email Box

--- lib/Login.dart
+++ lib/Login.dart
@@ -39,7 +39,18 @@ class _LoginState extends State<Login> {
                       child: Image.asset('assets/Logo.png')),
                 ),
               ),
-                ],
+              const Padding(
+                //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Email',
+                      hintText: 'Enter valid email id as abc@gmail.com'),
+                ),
+              ),
+
+            ],
           )
     ),
     );
