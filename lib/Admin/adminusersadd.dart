7f47011d6c7ce40a893d1ca370fba90bd8dba866
deepali2696 <deepali26996@gmail.com>
2022-11-23 23:12:02 -0500

    List User button added to adminusersadd.dart file

--- lib/Admin/adminusersadd.dart
+++ lib/Admin/adminusersadd.dart
@@ -18,90 +18,129 @@ class _adminusersadd extends State<adminusersadd> {
       backgroundColor: Color(0xFF7AB4BD),
       body: SingleChildScrollView(
           child: Column(children: <Widget>[
-        Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
+        Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
           Container(
             child: const screentitle(
               title: 'Admin',
             ),
           )
         ]),
-            Column(
-              children: const <Widget>[
-                imagecard(),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget> [
-                Container(
-                  color: Color(0xFFc3eaf0),
-                  child: Row(
-                    children: const <Widget>[
-                      ButtonBar(
-                        alignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          menuoption(menutext: 'Users', padlef: 10.0, autofocus: true,),
-                          menuoption(menutext: 'Stocks'),
-                          menuoption(menutext: 'Orders'),
-                        ],
-                      )
-                    ],
+        Column(
+          children: const <Widget>[
+            imagecard(),
+          ],
+        ),
+        Container(
+          color: Color(0xFFc3eaf0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const <Widget>[
+              ButtonBar(
+                alignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  menuoption(
+                    menutext: 'Users',
+                    padlef: 10.0,
+                    autofocus: true,
                   ),
-                  ),
-              ]
+                  menuoption(menutext: 'Stocks'),
+                  menuoption(menutext: 'Orders'),
+                ],
+              )
+            ],
+          ),
+        ),
+        Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.fromLTRB(30.0, 15.0, 0.0, 0.0),
+              alignment: Alignment.centerLeft,
+              child: const Text(
+                'Add User',
+                style: TextStyle(
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+              ),
+            )
+          ],
+        ),
+        Column(
+          children: const <Widget>[
+            textformfield(
+              Textfieldtitle: 'User Id',
             ),
-            Column(
-              children:  <Widget>[
-                Container(
-                  padding: EdgeInsets.fromLTRB(30.0, 15.0, 0.0, 0.0),
-                  child: Text('Add User',
-                    style: TextStyle(
-                        fontSize: 15.0,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black
+            textformfield(
+              Textfieldtitle: 'First Name',
+            ),
+            textformfield(
+              Textfieldtitle: 'Last Name',
+            ),
+            textformfield(
+              Textfieldtitle: 'Email',
+            ),
+            textformfield(
+              Textfieldtitle: 'Phone Number',
+            ),
+            textformfield(
+              Textfieldtitle: 'Password',
+            ),
+          ],
+        ),
+
+        Row(
+          children: <Widget>[
+            Container(
+                padding: EdgeInsets.fromLTRB(50.0, 15.0, 30.0, 0.0),
+                child: SizedBox(
+                  height: 40.0,
+                  width: 90.0,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.yellow,
+                    ),
+                    child: const Text(
+                      'Save',
+                      style: TextStyle(color: Colors.black87, fontSize: 15.0),
                     ),
                   ),
-                )
-              ],
-            ),
-            Column(
-              children: const <Widget>[
-                textformfield(Textfieldtitle: 'User Id',),
-                textformfield(
-                  Textfieldtitle: 'First Name',
-                ),
-                textformfield(
-                  Textfieldtitle: 'Last Name',
-                ),
-                textformfield(
-                  Textfieldtitle: 'Email',
-                ),
-                textformfield(
-                  Textfieldtitle: 'Phone Number',
-                ),
-                textformfield(
-                  Textfieldtitle: 'Password',
-                ),
-              ],
-            ),
+                )),
+
             Container(
-              padding: EdgeInsets.only(top: 15.0),
-              child: SizedBox(
-                height: 40.0,
-                width: 90.0,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: TextButton.styleFrom(
-                    backgroundColor: Colors.yellow,
+                padding: EdgeInsets.fromLTRB(50.0, 15.0, 30.0, 0.0),
+                child: SizedBox(
+                  height: 40.0,
+                  width: 110.0,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.yellow,
+                    ),
+                    child: Text(
+                      'List User',
+                      style: TextStyle(color: Colors.black87, fontSize: 15.0),
+                    ),
                   ),
-                  child: Text('Save',
-                  style: TextStyle(color: Colors.black87, fontSize: 15.0),
-                  ),
+                ))
+          ],
+        )
+        /*Container(
+            padding: EdgeInsets.only(top: 15.0),
+            child: SizedBox(
+              height: 40.0,
+              width: 90.0,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.yellow,
                 ),
-              )
-            )
+                child: Text(
+                  'Save',
+                  style: TextStyle(color: Colors.black87, fontSize: 15.0),
+                ),
+              ),
+            ))*/
       ])),
     );
   }
