2a07975be1b079f3a9700b4641adc8e3b8a8f300
Hardik_5894 <Vaviya.365@gmail.com>
2022-11-25 07:49:32 -0500

    login configured

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: stockproducts(),
+      home: Login(),
     );
   }
 }
